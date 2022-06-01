a1b4518df075623e2146560e1d33de9dc8752666
Vladislav <8269669@gmail.com>
2022-06-02 01:58:49 +0300

    Подстановка и Вернам

---
+++ vernam.py
@@ -0,0 +1,9 @@
+import math
+text = input()
+key = input()
+alph = "qwertyuiopasdfghjklzxcvbnm_{}1234567890"
+key = key * math.ceil(len(text)/len(key))
+ans = ""
+for i in range(len(text)):
+    ans += alph[((alph.find(text[i]) + 1) + (alph.find(key[i]) + 1)) % len(alph) - 2]
+print(ans)
