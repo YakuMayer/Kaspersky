1d7ec35e8a35e3b454b4acc93aaa2cb9832b6ea3
YakuMayer <100630625+YakuMayer@users.noreply.github.com>
2022-06-03 12:33:34 +0300

    Update vernam.py

--- vernam.py
+++ vernam.py
@@ -5,5 +5,5 @@ alph = "qwertyuiopasdfghjklzxcvbnm_{}1234567890"
 key = key * math.ceil(len(text)/len(key))
 ans = ""
 for i in range(len(text)):
-    ans += alph[((alph.find(text[i]) + 1) + (alph.find(key[i]) + 1)) % len(alph) - 2]
+    ans += alph[((alph.find(text[i]) + 1) + (alph.find(key[i]) + 1)) % len(alph) - 1]
 print(ans)
