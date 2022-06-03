5e2b90d46360b4801efbbfe93a92c4e1f52c960c
YakuMayer <100630625+YakuMayer@users.noreply.github.com>
2022-06-03 16:17:35 +0300

    Update ceaser.py

--- ceaser.py
+++ ceaser.py
@@ -3,15 +3,18 @@ alph_up = "".join(chr(i) for i in range(ord("A"), ord("Z") + 1))
 alph_low = "".join(chr(i) for i in range(ord("a"), ord("z") + 1))
 questions = []
 text0 = text.split("?")
-text = []
+dot = 0
+dots = []
 for a in text0:
     q = a.split(".")
-    questions.append(q[len(q)-1])
+    questions.append(q[-1])
     a = a.replace(q[len(q)-1], "")
-    text.append(a)
-text.pop(0)
-text.append("")
-print(text)
+for b in range(len(text)):
+    if text[b] == ".":
+        dot = b
+    if text[b] == "?":
+        dots.append(dot)
+dots.append(len(text))
 answer = ""
 for i in range(len(questions)):
     for x in range(27):
@@ -28,14 +31,14 @@ for i in range(len(questions)):
         result = input()
         if result == "y":
             ans = ""
-            print(i)
-            for z in text[i]:
-                if z in alph_up:
-                    ans += alph_up[(alph_up.index(z) - x)%26]
-                elif z in alph_low:
-                    ans += alph_low[(alph_low.index(z) - x)%26]
-                else:
-                    ans += z
-            answer += ans + "?"
+            if i+1  <  len(dots):
+                for z in text[dots[i]:dots[i+1]]:
+                    if z in alph_up:
+                        ans += alph_up[(alph_up.index(z) - x)%26]
+                    elif z in alph_low:
+                        ans += alph_low[(alph_low.index(z) - x)%26]
+                    else:
+                        ans += z
+            answer += ans
             break
 print(answer)
